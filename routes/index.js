b902b8cc5b4be1024c22c20fbf12fa0d1cd77a33
Dheeraj kumar <yadavdheeraj197@gmail.com>
2020-08-19 22:56:45 +0530

    tr

--- routes/index.js
+++ routes/index.js
@@ -30,60 +30,70 @@ router.post('/info',function(req,res){
    }
  });
 });
+// Student.findOne({function(err, record){
+//   res.render("record", {
+//     name: record.name,
+//     branch: record.branch
+//   });
+// });
 
-router.get("/record" , function(req,res){
-   res.render("record");
-});
-
-router.get("/library" , function(req,res){
-   res.render("library");
-});
-router.post('/library',function(req,res){
- const library= new student({
-   rollno:req.body.studentRollno,
-   branch:req.body.studentBranch
- });
- library.save(function(err){
-   if (!err){
-       res.redirect("/acedmics");
-   }
- });
+router.get("/record", function(req, res){
+  Student.find({}, function(err, records){
+    res.render("record", {
+      records: records
+      });
+  });
 });
 
-router.get("/acedmics" , function(req,res){
-   res.render("acedmics");
-});
-router.post('/acedmics',function(req,res){
- const acedmics= new student({
-   rollno:req.body.studentRollno,
-   DUAcd:req.body.DUAcadmics,
-   DUAcdAmt:req.body.acadmicsAmount
- });
- acedmics.save(function(err){
-   if (!err){
-       res.redirect("/hostel");
-   }
- });
-});
-
-router.get("/hostel" , function(req,res){
-   res.render("hostel");
-});
-router.post('/hostel',function(req,res){
- const hostel= new student({
-   rollno:req.body.studentRollno,
-   DUHostel:req.body.DUHostel,
-   DUHostelAmt:req.body.hostelAmount
- });
- hostel.save(function(err){
-   if (!err){
-       res.redirect("/success");
-   }
- });
-});
-
-router.get("/success" , function(req,res){
-   res.render("success");
-});
+// router.get("/library" , function(req,res){
+//    res.render("library");
+// });
+// router.post('/library',function(req,res){
+//  const library= new student({
+//    rollno:req.body.studentRollno,
+//    branch:req.body.studentBranch
+//  });
+//  library.save(function(err){
+//    if (!err){
+//        res.redirect("/acedmics");
+//    }
+//  });
+// });
+//
+// router.get("/acedmics" , function(req,res){
+//    res.render("acedmics");
+// });
+// router.post('/acedmics',function(req,res){
+//  const acedmics= new student({
+//    rollno:req.body.studentRollno,
+//    DUAcd:req.body.DUAcadmics,
+//    DUAcdAmt:req.body.acadmicsAmount
+//  });
+//  acedmics.save(function(err){
+//    if (!err){
+//        res.redirect("/hostel");
+//    }
+//  });
+// });
+//
+// router.get("/hostel" , function(req,res){
+//    res.render("hostel");
+// });
+// router.post('/hostel',function(req,res){
+//  const hostel= new student({
+//    rollno:req.body.studentRollno,
+//    DUHostel:req.body.DUHostel,
+//    DUHostelAmt:req.body.hostelAmount
+//  });
+//  hostel.save(function(err){
+//    if (!err){
+//        res.redirect("/success");
+//    }
+//  });
+// });
+//
+// router.get("/success" , function(req,res){
+//    res.render("success");
+// });
 
 module.exports = router;
